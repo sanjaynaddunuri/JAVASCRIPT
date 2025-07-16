f432f1285718cc3a2e8af4b06354751f79aacfdc
Naddunuri Sanjay <204818477+sanjaynaddunuri@users.noreply.github.com>
2025-07-16 11:02:55 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,15 @@
+var city;
+city="Warangal";
+var name;
+name="SanjuShree";
+var email;
+email="naddunurisanjay@gmail.com"
+let x;
+x=10;
+let y;
+y=20;
+var phone;
+phone="83232";
+
+var id=120;
+
---
+++ while.js
@@ -0,0 +1,12 @@
+function firsttennumbers()
+{
+    var x=1
+    console.log("Loop is started")
+    while (x<=10)
+    {
+        console.log(x);
+        x++ 
+    }
+     console.log("Loop is Ended")
+}
+firsttennumbers()
